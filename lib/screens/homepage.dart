05d4bce7c9ac1ec4c19a10dcd70acdf247293517
unknown <sajanmanandhar12@gmail.com>
2023-07-07 14:18:28 +0545

    changes

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,78 +12,92 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: Consumer(
-        builder: (context, ref, child) {
-          final productData = ref.watch(productProvider);
-          return Container(
-            padding: EdgeInsets.all(14),
-            child: Column(
-              children: [
-                Text(
-                  'Welcome to Online Store',
-                  style: TextStyle(fontSize: 30),
-                ),
-                Container(
-                  color: Color.fromARGB(255, 173, 173, 173),
-                  child: TextField(
-                    onTap: () {
-                      Get.to(SearchPage());
-                    },
-                    decoration: InputDecoration(hintText: 'Search items'),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                    child: GridView.builder(
-                  itemCount: productData.products.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisSpacing: 12,
-                      mainAxisExtent: 280,
-                      mainAxisSpacing: 12,
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    return Container(
-                      padding: EdgeInsets.all(10),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text(
+                'Online Store',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Consumer(
+            builder: (context, ref, child) {
+              final productData = ref.watch(productProvider);
+              return Container(
+                padding: EdgeInsets.all(14),
+                child: Column(
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                      margin: EdgeInsets.symmetric(horizontal: 10),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(12),
-                          color: Color.fromARGB(153, 240, 240, 240)),
-                      child: Column(children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: Image(
-                              height: 150,
-                              image: NetworkImage(
-                                  productData.products[index].image)),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(productData.products[index].title,
-                                maxLines: 2,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            SizedBox(
-                              height: 10,
+                          border: Border.all(color: Colors.grey)),
+                      child: TextField(
+                        onTap: () {
+                          Get.to(SearchPage());
+                        },
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Search items'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                        child: GridView.builder(
+                      itemCount: productData.products.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisSpacing: 12,
+                          mainAxisExtent: 280,
+                          mainAxisSpacing: 12,
+                          crossAxisCount: 2),
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Color.fromARGB(153, 240, 240, 240)),
+                          child: Column(children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image(
+                                  height: 150,
+                                  image: NetworkImage(
+                                      productData.products[index].image)),
                             ),
-                            Text('\$' + productData.products[index].price,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.lightGreen))
-                          ],
-                        ),
-                      ]),
-                    );
-                  },
-                )),
-              ],
-            ),
-          );
-        },
-      )),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(productData.products[index].title,
+                                    maxLines: 2,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text('\$' + productData.products[index].price,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.lightGreen))
+                              ],
+                            ),
+                          ]),
+                        );
+                      },
+                    )),
+                  ],
+                ),
+              );
+            },
+          )),
     );
   }
 }
